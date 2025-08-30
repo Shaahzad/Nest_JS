438d07db418ec94cc2a2a123c1327c993a83cefe
Shaahzad <shehzadmemon147@gmail.com>
2025-08-30 23:28:49 +0500

    learning custom pipe

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CustomerService } from './customer.service';
+import { CreateCustomerDto } from './dto/create-customer.dto';
 
 @Controller('customer')
 export class CustomerController {
-    constructor(private readonly customerService: CustomerService){}
+    constructor(private readonly customerService: CustomerService) { }
 
     @Get()
-    getCustomer(){
-        
+    getCustomer() {
+        return this.customerService.getAllCustomer()
+    }
+
+    @Post()
+    addCustomer(@Body() CreateCustomerDto: CreateCustomerDto) {
+        this.customerService.addcustomer(CreateCustomerDto)
     }
 
 }
