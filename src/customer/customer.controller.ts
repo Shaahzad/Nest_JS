6b51581ae59cec8fd4fbfdf06858945c837922bf
Shaahzad <shehzadmemon147gmail.com>
2025-08-30 22:05:46 +0500

    Re-init project

---
+++ src/customer/customer.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { CustomerService } from './customer.service';
+
+@Controller('customer')
+export class CustomerController {
+    constructor(private readonly customerService: CustomerService){}
+
+    @Get()
+    getCustomer(){
+        
+    }
+
+}
---
+++ src/customer/customer.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { Customer } from './interfaces/customer.interface';
+import { CreateCustomerDto } from './dto/create-customer.dto';
+
+@Injectable()
+export class CustomerService {
+    private customers: Customer[] = []
+
+    getAllCustomer(): Customer[] {
+        return this.customers
+    }
+
+    addcustomer(createCustomerDto: CreateCustomerDto): Customer {
+        const newCustomer: Customer = {
+            id: Date.now(),
+            ...createCustomerDto
+        };
+        this.customers.push(newCustomer)
+        return newCustomer
+    }
+}
