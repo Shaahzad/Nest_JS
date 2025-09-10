4064748058d7e9260f280786b0879442e1289024
Shaahzad <shehzadmemon147@gmail.com>
2025-09-10 23:45:57 +0500

    Protect Routes using Guards

--- src/ny-name/ny-name.controller.ts
+++ src/ny-name/ny-name.controller.ts
@@ -1,6 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UppercasePipe } from 'src/common/pipes/uppercase/uppercase.pipe';
 
 @Controller('ny-name')
 export class NyNameController {
-    
+    @Post('custom')
+    transformName(@Body('name', new UppercasePipe()) name: string){
+        return {message: `Received name: ${name}`}
+    }
 }
